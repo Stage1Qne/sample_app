eb0e954eec412197575b5320a04a6f03dae5622e
stage1qne <stage1qne@gmail.com>
2013-09-06 10:29:19 +0300

    Add user microposts

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,7 +8,7 @@ require 'spec_helper'
 
     let(:user) { FactoryGirl.create(:user) }
 
-    before(:each) do
+    before do
       sign_in user
       visit users_path
     end
@@ -62,6 +62,23 @@ require 'spec_helper'
 
     it { should have_selector('h1',    text: user.name) }
     it { should have_selector('title', text: user.name) }
+  end
+
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
+    before { visit user_path(user) }
+
+    it { should have_selector('h1',    text: user.name) }
+    it { should have_selector('title', text: user.name) }
+
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
   end
   
   describe "signup" do
